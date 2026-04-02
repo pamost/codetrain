4c1e0d81b0005f0c711dd3fe22461deec957e7ed
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2026-04-02 10:49:18 +0300

    Registering user for admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from app.models import Language, Topic
+from app.models import User, Language, Topic
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'created_at')
+    search_fields = ('username', 'email')
+    readonly_fields = ('created_at',)
+    list_filter = ('created_at',)
 
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
@@ -14,4 +21,3 @@ class TopicAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
     list_editable = ('order',)
-
